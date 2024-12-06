dfabc1994ddbafb8eb4a473616d1d33078da0b99
Ian Ulrich <72941389+IanU26@users.noreply.github.com>
2024-12-05 20:55:27 -0800

    Update temp_converter.v

--- temp_converter.v
+++ temp_converter.v
@@ -1,15 +1,37 @@
 `timescale 1ns / 1ps
-//This module converts celcius to fahrenheit. F = 9/5*C+32
-module temp_converter(
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/03/2024 08:31:00 PM
+// Design Name: 
+// Module Name: tempConverter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tempConverter(
     input [7:0] c,
-    output [7:0] f
+    output reg[7:0] f
     );
     
-    wire [15:0] p;
-    wire [7:0] q;
+    reg [15:0] a;
+    reg [7:0] b,d;
     
-    multiply_by_9 MULT9(.x(c), .y(p));
-    divide_by_5 DIV5(.x(p), .y(q));
-    add_32 ADD32(.x(q), .y(f));
+    always@(*)begin
+        a = c * 9;
+        b = a / 5;
+        f = b + 32;
+    end
     
 endmodule
