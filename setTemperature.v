9291f30fbb4d217e54065a8f11efac5b95274662
Ian Ulrich <72941389+IanU26@users.noreply.github.com>
2024-12-06 19:01:14 -0800

    Update setTemperature.v

--- setTemperature.v
+++ setTemperature.v
@@ -9,10 +9,10 @@ module setTemperature(
     input [1:0]   selectionSW,   // Used to select which output to display. Bind to switches
     input         CLK100MHZ,        // nexys clk signal
     input [7:0] set_temp,             // Set temp will be set using the left most swtiches
-    input [7:0] ave_temp,             // Temp data from i2c master
-    input [7:0] max_temp,             // Temp data from i2c master
-    input [7:0] min_temp,             // Temp data from i2c master
-    output reg[7:0] display_reg               // nexys leds = binary temp in deg F, 15-8 (set temp) 7-0 (current temp)
+    input [7:0] ave_temp,             // Temp data from bram_controller
+    input [7:0] max_temp,             
+    input [7:0] min_temp,             
+    output reg[7:0] display_reg           
     );
     
     always@(posedge CLK100MHZ)begin
